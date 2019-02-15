1e4f1c9c3fd88bdb4b27f1f37eb4782ad563e73b
benlegendj <benlegendj@gmail.com>
2019-02-15 23:22:17 +0300

    implement local storage on todo app

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,30 +1,15 @@
 // DOM manipulation --> Document Object Model
-const notes = [{
-    title: 'learn javascript',
-    body: 'know how to implement fetch api'
-}, {
-    title: 'learn python',
-    body: 'know how to implement jwt extended'
-}, {
-    title: 'learn css',
-    body: 'know how to implement flex boxes'
-}]
+let notes = []
 
 const filters = {
     searchText: ''
 }
-const userObj = {
-    name: 'benedict',
-    age: 25
+// check existing notes
+const notesJSON = localStorage.getItem('notes')
+
+if (notesJSON !== null) {
+    notes = JSON.parse(notesJSON)
 }
-// stringfy --> creates a proper json objects using double quotes 
-// parse --> convers json into js object
-userJson = JSON.stringify(userObj)
-
-const userJsonified = localStorage.getItem(user)
-const user = JSON.parse(userJsonified)
-
-console.log(`my name is ${user.name} and iam ${user.age}`)
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -34,11 +19,30 @@ const renderNotes = function (notes, filters) {
 
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
+
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title
+        }
+        else {
+            noteEl.textContent = 'Unnamed note'
+        }
+
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
+
 renderNotes(notes, filters)
+
+
+document.querySelector('#create-notes').addEventListener('click', function (e) {
+    notes.push({
+        title: '',
+        body: ''
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    renderNotes(notes, filters)
+})
+
 document.querySelector('#search').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
