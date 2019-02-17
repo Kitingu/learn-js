bb3da9ec093d9b3a0abb03805d393599d4e4e046
benlegendj <benlegendj@gmail.com>
2019-02-17 22:09:38 +0300

    refactor notes app

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,45 +1,19 @@
 // DOM manipulation --> Document Object Model
-let notes = []
+let notes = getSaveNotes()
 
 const filters = {
     searchText: ''
 }
 // check existing notes
-const notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-
-        if (note.title.length > 0) {
-            noteEl.textContent = note.title
-        }
-        else {
-            noteEl.textContent = 'Unnamed note'
-        }
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
 
 renderNotes(notes, filters)
-
 
 document.querySelector('#create-notes').addEventListener('click', function (e) {
     notes.push({
         title: '',
         body: ''
     })
-    localStorage.setItem('notes', JSON.stringify(notes))
+    saveNotes(notes)
     renderNotes(notes, filters)
 })
 
