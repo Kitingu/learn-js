c602e3b4cc176abb0835970fef66252af6e31225
benlegendj <benlegendj@gmail.com>
2019-03-22 23:10:58 +0300

    using prototypal inheritance

--- oop/person.js
+++ oop/person.js
@@ -1,10 +1,30 @@
-const Person = function (firstName, lastName, age) {
+// prototypal inheritance
+// protype fuctions are accessible to all instances of the class i.e methods
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
 }
-const kasee = new Person('charles','mwendwa',13)
-const me = new Person('benedict','mwendwa',25)
-me.lastName = 'kitingu'
-console.log(me)
-console.log(kasee)
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+
+    this.likes.forEach((like) => {
+        bio = bio + ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+
+const kasee = new Person('charles', 'mwendwa', 13)
+const me = new Person('benedict', 'mwendwa', 25, ['watching','dancing'])
+me.setName('stephen kitingu')
+console.log(me.getBio())
+
+console.log(kasee.getBio())
