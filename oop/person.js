fbd1232c0db9027bbeb3cd3cf83a9d063a16ae84
benlegendj <benlegendj@gmail.com>
2019-03-29 10:39:55 +0300

    use class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,29 +1,34 @@
 // prototypal inheritance
 // protype fuctions are accessible to all instances of the class i.e methods
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
 
-    this.likes.forEach((like) => {
-        bio = bio + ` ${this.firstName} likes ${like}.`
-    })
-    return bio
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
 
 
 const kasee = new Person('charles', 'mwendwa', 13)
-const me = new Person('benedict', 'mwendwa', 25, ['watching','dancing'])
+const me = new Person('benedict', 'mwendwa', 25, ['watching', 'dancing'])
 me.setName('stephen kitingu')
 console.log(me.getBio())
 
