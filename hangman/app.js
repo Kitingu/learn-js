1a21a90177c32397b877f8e1c6d59a51bd5bd50a
benlegendj <benlegendj@gmail.com>
2019-03-31 18:18:47 +0300

    http  headers and errors

--- hangman/app.js
+++ hangman/app.js
@@ -10,7 +10,7 @@ const game1 = new Hangman('bad mind', 2)
 puzzleEl.textContent = game1.puzzle
 guessEl.textContent = game1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGues(guess)
     puzzleEl.textContent = game1.puzzle
@@ -19,11 +19,14 @@ window.addEventListener('keypress', function (e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+        console.log(e.target.status)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has occured')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
 request.send()
