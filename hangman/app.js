4a6423efc504b4a4afdfa05ecce041298bb7d7fd
benlegendj <benlegendj@gmail.com>
2019-04-01 23:36:40 +0300

    intro to callback abstarction

--- hangman/app.js
+++ hangman/app.js
@@ -16,38 +16,36 @@ window.addEventListener('keypress', (e) => {
     puzzleEl.textContent = game1.puzzle
     guessEl.textContent = game1.statusMessage
 })
-const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-        console.log(e.target.status)
-    } else if (e.target.readyState === 4) {
-        console.log('An error has occured')
+
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    }
+    else {
+        console.log(puzzle)
     }
 })
 
 
-const countryRequest = new XMLHttpRequest()
-const countryCode = 'KE'
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find(country => country.alpha2Code === countryCode)
-        console.log(country.name)
-        // data.every((country) => {
-        //     console.log(country)
-        // })
-    }
-    else if (e.target.readyState === 4) {
-        console.log('Unable to fetch data')
-    }
-})
+
+// const countryRequest = new XMLHttpRequest()
+// const countryCode = 'KE'
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find(country => country.alpha2Code === countryCode)
+//         console.log(country.name)
+//         // data.every((country) => {
+//         //     console.log(country)
+//         // })
+//     }
+//     else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data')
+//     }
+// })
 
 
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-countryRequest.send()
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
-request.send()
