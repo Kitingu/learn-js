a18a8609df64da53c6f8afc7226b20f7f4de7833
benlegendj <benlegendj@gmail.com>
2019-04-05 09:21:00 +0300

    fetch api challenge

--- hangman/app.js
+++ hangman/app.js
@@ -28,26 +28,10 @@ getPuzzle('2').then((puzzle) => {
 
 
 
-// getCountry('KE').then((country) => {
-//     console.log(`Country Name:  ${country.name}`)
+getCountry('KE').then((country) => {
+    console.log(`Country Name:  ${country.name}`)
 
-// }
-//     , (error) => {
-//         console.log(`Error: ${error}`)
-//     })
+}).catch((error) => {
+        return `Error: ${error}`
+    })
 
-
-
-
-// intro to fetch api
-// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-//     if (response.status === 200) {
-//         return response.json() //this is a promise
-//     } else {
-//         throw new Error('Unable to fetch the puzzle')
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle)
-// }).catch((error) => {
-//     console.log(error)
-// })
--- hangman/requests.js
+++ hangman/requests.js
@@ -7,26 +7,22 @@ const getPuzzle = (wordCount) => {
             throw new Error('puzzle could not be fetched')
         }
 
-    }).then((data) =>{
-        return data.string
+    }).then((data) => {
+        return data.puzzle
     })
 }
 
-const getCountry = (countryCode) => new Promise((resolve, reject) => {
-    const countryRequest = new XMLHttpRequest()
-
-    countryRequest.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            const country = data.find(country => country.alpha2Code === countryCode)
-            resolve(country)
-
+const getCountry = (countryCode) => {
+    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+        if (response.status === 200) {
+            return response.json()
         }
-        else if (e.target.readyState === 4) {
-            reject('An error has occured', undefined)
+        else {
+            throw new Error('Unable to fetch a country')
         }
+    }).then((data) => {
+        return data.find(country => country.alpha2Code === countryCode)
+        
     })
-
-    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-    countryRequest.send()
-})
+}
+1
