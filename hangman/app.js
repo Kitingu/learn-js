c857b1d203c753027b9ad601220489ef019d9e30
benlegendj <benlegendj@gmail.com>
2019-04-05 08:59:49 +0300

    use fetch api to get puzzle

--- hangman/app.js
+++ hangman/app.js
@@ -18,11 +18,11 @@ window.addEventListener('keypress', (e) => {
 })
 
 
-// getPuzzle('2').then((puzzle) => {
-//     console.log(puzzle)
-// }, (error) => {
-//     console.log(`Error: ${error}`)
-// })
+getPuzzle('2').then((data) => {
+    console.log(data.puzzle)
+}).catch((error) => {
+    console.log(`Error: ${error}`)
+})
 
 
 
@@ -40,14 +40,14 @@ window.addEventListener('keypress', (e) => {
 
 
 // intro to fetch api
-fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-    if (response.status === 200) {
-        return response.json() //this is a promise
-    } else {
-        throw new Error('Unable to fetch the puzzle')
-    }
-}).then((data) => {
-    console.log(data.puzzle)
-}).catch((error) => {
-    console.log(error)
-})
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if (response.status === 200) {
+//         return response.json() //this is a promise
+//     } else {
+//         throw new Error('Unable to fetch the puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
