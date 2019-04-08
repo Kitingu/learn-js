95a53e29bb54a3f5abc894a8fc2b7a55a2f2d570
benlegendj <benlegendj@gmail.com>
2019-04-08 23:44:13 +0300

    setup hangman theme and favicon

--- hangman/scripts/app.js
+++ hangman/scripts/app.js
@@ -7,22 +7,32 @@ const guessEl = document.querySelector('#guesses')
 
 let game1
 
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+
+const render = () => {
+    puzzleEl.innerHTML = ''
+    guessEl.textContent = game1.statusMessage
+
+    game1.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        puzzleEl.appendChild(letterEl)
+    })
+}
+
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGues(guess)
     render()
 })
 
-const render = () => {
-    puzzleEl.textContent = game1.puzzle
-    guessEl.textContent = game1.statusMessage
-}
 
-const startGame = async () => {
-    const puzzle = await getPuzzle('2')
-    game1 = new Hangman(puzzle, 5)
-    render()
-}
+
 
 document.querySelector('#reset').addEventListener('click', startGame)
 startGame()
