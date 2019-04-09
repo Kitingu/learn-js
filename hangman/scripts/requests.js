5116996c5ff475e12a094d42bd8c177d82c4af35
benlegendj <benlegendj@gmail.com>
2019-04-09 13:53:36 +0300

    remove http flagging on urls

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,6 +1,6 @@
 const getPuzzle = async (wordCount) => {
 
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
     if (response.status === 200) {
 
@@ -16,7 +16,7 @@ const getPuzzle = async (wordCount) => {
 
 
 const getCountry = async (countryCode) => {
-    const response = await fetch('http://restcountries.eu/rest/v2/all')
+    const response = await fetch('//restcountries.eu/rest/v2/all')
     if (response.status === 200) {
         const data = await response.json()
         return data.find(country => country.alpha2Code === countryCode)
@@ -29,7 +29,7 @@ const getCountry = async (countryCode) => {
 
 
 const getLocation = async () => {
-    const response = await fetch("http://ipinfo.io/json?token=9abccb200d16ef")
+    const response = await fetch("://ipinfo.io/json?token=9abccb200d16ef")
     if (response.status === 200) {
         return response.json()
     }
