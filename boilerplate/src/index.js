4bf6371c97f477ebaf51df5129d22625415c8800
benlegendj <benlegendj@gmail.com>
2019-04-13 00:32:48 +0300

    node mudules challenge

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,5 +1,7 @@
 import './utilities'
 import { add } from './utilities' //if many you can comma separate them
+import { scream } from './scream'
 console.log('index')
 
 console.log(add(10, 5))
+console.log(scream('jontez'))
---
+++ boilerplate/src/scream.js
@@ -0,0 +1,1 @@
+export const scream = (name) => `${name.toUpperCase()}!!!`
