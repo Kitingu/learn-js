48dd92717b0a3aaefc87634853241f29d83bd517
benlegendj <benlegendj@gmail.com>
2019-04-15 13:57:40 +0300

    convert notes app part 3

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,6 +1,15 @@
-import { getNotes,createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import {getFilters,setFilters} from './filters'
 
-
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// updateNote('dbaa5d57-c68f-46cb-9451-93b291d7161d',{
+//     'title':'new body',
+//     body:'nothing was the same'
+// })
+// console.log(getNotes())
+console.log(getFilters())
+setFilters({
+    searchText: 'Office',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
